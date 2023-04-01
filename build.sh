2edd719cf7825e95e520d971f1803020d5df0ed1
k0d3r1s <k0d3r1s@gmail.com>
2023-04-02 02:24:29 +0300

    refactor: cleanup

--- build.sh
+++ build.sh
@@ -24,12 +24,9 @@ export ARCH
 
 docker login ghcr.io -u $CR_USER --password $CR_PAT
 
-update.sh
-
-docker buildx build --tag ghcr.io/960018/debian:base-$ARCH --push --compress --no-cache --sbom=false --provenance=false -f Dockerfile.debian.base . || exit
-docker buildx build --tag ghcr.io/960018/debian:curl-$ARCH --push --compress --no-cache --sbom=false --provenance=false -f Dockerfile.debian.curl --build-arg OS=$ARCH . || exit
-docker buildx build --tag ghcr.io/960018/debian:nginx-$ARCH --push --compress --no-cache --sbom=false --provenance=false -f Dockerfile.debian.nginx . || exit
-docker buildx build --tag ghcr.io/960018/debian:keydb-$ARCH --push --compress --no-cache --sbom=false --provenance=false -f Dockerfile.debian.keydb --build-arg VERSION=latest . || exit
-docker buildx build --tag ghcr.io/960018/debian:node-$ARCH --push --compress --no-cache --sbom=false --provenance=false -f Dockerfile.debian.node . || exit
-docker buildx build --tag ghcr.io/960018/debian:php-fpm-$ARCH --push --compress --no-cache --sbom=false --provenance=false -f Dockerfile.debian.php --build-arg RELAY=$RELAY --build-arg OS=$ARCH . || exit
-docker buildx build --tag ghcr.io/960018/debian:php-fpm-testing-$ARCH --push --compress --no-cache --sbom=false --provenance=false -f Dockerfile.debian.php.testing --build-arg OS=$ARCH . || exit
+docker buildx build --tag ghcr.io/960018/curl:debian-$ARCH --push --compress --no-cache --sbom=false --provenance=false -f Dockerfile.debian.curl . || exit
+docker buildx build --tag ghcr.io/960018/nginx:debian-$ARCH --push --compress --no-cache --sbom=false --provenance=false -f Dockerfile.debian.nginx . || exit
+docker buildx build --tag ghcr.io/960018/keydb:debian-$ARCH --push --compress --no-cache --sbom=false --provenance=false -f Dockerfile.debian.keydb --build-arg VERSION=latest . || exit
+docker buildx build --tag ghcr.io/960018/node:debian-$ARCH --push --compress --no-cache --sbom=false --provenance=false -f Dockerfile.debian.node . || exit
+docker buildx build --tag ghcr.io/960018/php-fpm:debian-$ARCH --push --compress --no-cache --sbom=false --provenance=false -f Dockerfile.debian.php --build-arg RELAY=$RELAY --build-arg OS=$ARCH . || exit
+docker buildx build --tag ghcr.io/960018/php-fpm:debian-$ARCH-testing --push --compress --no-cache --sbom=false --provenance=false -f Dockerfile.debian.php.testing --build-arg OS=$ARCH . || exit
